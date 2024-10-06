946903d5dda4a58a20821325dccea58564ddc299
IvanYAIT <YAkushikia20@st.ithub.ru>
2024-10-06 11:19:36 +0300

    add new scene

--- LudumDare56/Assets/_Source/Player/PickUpSystem.cs
+++ LudumDare56/Assets/_Source/Player/PickUpSystem.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUpSystem : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PickUpSystem
     {
-        
-    }
+        public void PickUp(Transform itemForPick, Transform inventory)
+        {
+            itemForPick.parent = inventory;
+            itemForPick.localPosition = Vector3.zero;
+            itemForPick.rotation = Quaternion.identity;
+            itemForPick.GetComponent<Rigidbody>().isKinematic = true;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Drop(Transform pickedUpItem)
+        {
+            pickedUpItem.parent = null;
+            pickedUpItem.GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
